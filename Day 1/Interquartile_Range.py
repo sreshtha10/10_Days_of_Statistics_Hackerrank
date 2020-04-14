08f7744fb1e9f75448e913ac57618576bdd2fc63
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-14 14:39:39 +0530

    Update Interquartile_Range.py

--- Day 1/Interquartile_Range.py
+++ Day 1/Interquartile_Range.py
@@ -1,34 +1,43 @@
 n = int(input())
 elements = list(map(int,input().split()))
-data = sorted(elements)
-def median(element,n):
+frequency = list(map(int,input().split()))
+data = []
+for i in range(int(len(elements))):
+    x = 0
+    while x < frequency[i]:
+        data.append(elements[i])
+        x+=1
+data = sorted(data)
+n = len(data)
+def median(element,x):
     median = 0
     index = 0
     element = sorted(element)
-    if (n)%2 != 0:
-        index = (n+1)/2
+    if (x)%2 != 0:
+        index = (x+1)/2
         median = element[int(index-1)]
-        return int(median)
+        return float(median)
     else:
-        index = n/2
+        index = x/2
         median = element[int(index)]+element[int(index-1)]
-        return int(median/2)      
+        return (median/float(2))      
 q1 =[]
 q2 = []
 if n%2 == 0:
     for i in range(0,int(n/2)):
         q1.append(data[i])
-    print(median(q1,n/2))
-    print(median(data,n))
+    Q1 = (median(q1,n/2))
+
     for i in range(int(n/2),int(n)):
         q2.append(data[i])
-    print(median(q2,n/2))
+    Q3 = (median(q2,n/2))
 
 else:
     for i in range(0,int((n+1)/2)-1):
         q1.append(data[i])
-    print(median(q1,(n-1)/2))
-    print(median(data,n))
+    Q1 =(median(q1,(n-1)/2))
     for i in range((int((n+1)/2)),n):
         q2.append(data[i])
-    print(median(q2,(n-1)/2))
+    Q3 =(median(q2,(n-1)/2))
+
+print(round((Q3-Q1),1))
