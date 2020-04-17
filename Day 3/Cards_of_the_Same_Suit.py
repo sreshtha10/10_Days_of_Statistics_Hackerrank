f2829890e91b7b266eb4716fb08064d4bd27dc59
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-17 10:48:13 +0530

    Create Cards_of_the_Same_Suit.py

---
+++ Day 3/Cards_of_the_Same_Suit.py
@@ -0,0 +1,6 @@
+#Total cards are 52 , therefore each deck has 13 cards each.
+#probabilty of drawing any specific suit is 13/52 or 1/4
+#probability of drawing a card from that same suit will be 12/51 as 1 card is already taken
+#probability of each suit will be 13/52 * 12/51
+#As there are 4 suits in a deck, therefore total probability is 4 * 13/52 * 12/51
+print(12/51)
